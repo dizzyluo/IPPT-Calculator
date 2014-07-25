b45e7846a791d261400b7d483af41ed1747512f5
John Luo <johnldz@icloud.com>
2014-07-25 16:09:59 +0800

    Change Tracking

--- index.php
+++ index.php
@@ -153,19 +153,6 @@
 				points.total = points.sitUp + points.pushUp + points.running;
 				points.award = $scope.getAward(points.total);
 
-				if (points.sitUp > 0 && points.pushUp > 0 && points.running > 0) {
-					mixpanel.track("Calculate", {
-						age: p.age,
-						commando: p.commando,
-						sitUpCount: p.sitUpCount,
-						sitUp: points.sitUp,
-						pushUpCount: p.pushUpCount,
-						pushUp: points.pushUp,
-						running: points.running,
-						runningTime: time,
-						award: point.award
-					});
-				};
 			}
 
 			$scope.getAward = function (pts) {
@@ -212,6 +199,7 @@
 		</script>
 		<!-- start Mixpanel --><script type="text/javascript">(function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
 		for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement("script");a.type="text/javascript";a.async=!0;a.src="//cdn.mxpnl.com/libs/mixpanel-2.2.min.js";e=f.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);
-		mixpanel.init("c7e31f0de78ba726e76fc8dc57892cae");</script><!-- end Mixpanel -->
+		mixpanel.init("c7e31f0de78ba726e76fc8dc57892cae");
+		mixpanel.track("Visit")</script><!-- end Mixpanel -->
 	</body>
 </html>
